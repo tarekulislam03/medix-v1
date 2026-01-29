f9a21dbb7394dc61ce98ac87b229ac2e1031ab5f
Tarekul <tarekul03.muslim@gmail.com>
2026-01-29 19:37:28 +0530

    v1

--- pharmacy-pos-saas/frontend/vite.config.ts
+++ pharmacy-pos-saas/frontend/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
 
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://medix-v1-kvn5.vercel.app/',
         changeOrigin: true,
       },
     },
